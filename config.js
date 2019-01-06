2ccd8b98a310315271725cc26aab5d3b7c7894cf
Dominik Zając <zajac_dominik@onet.pl>
2019-01-06 15:45:33 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '23748374949-sahjo5ho73c1vc4m1mbl7gq3dmt03fq9.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'uceKEZ3R9mXbgKHAW3038UhI',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
